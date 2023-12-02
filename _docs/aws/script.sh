5835c52a313daef48e69f587939f2e5f88d0d247
Sanket Suresh Thorat <devilmind201603116@gmail.com>
2023-12-02 21:02:17 +0000

    week 0:2

--- _docs/aws/script.sh
+++ _docs/aws/script.sh
@@ -32,3 +32,9 @@ aws sns subscribe \
 
 # Create an Alarm for daily charges
 aws cloudwatch put-metric-alarm --cli-input-json file://_docs/aws/billing-alarm.json
+
+
+## User truffle-hog and bfg to look out for any secrets commited to remote repo either in present or history of the repo.
+
+################################# WEEk - 1 ####################################
+
